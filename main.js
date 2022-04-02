6c6174a1ef07deeeb4ab331cce9a5d4b8d311147
I-love-cupcakes <95037674+I-love-cupcakes@users.noreply.github.com>
2022-04-02 09:36:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,24 +8,66 @@ function start()
     recognition.start();
 }
 
-recognition.onresult = function (event) {
+recognition.onresult = function (event) 
+{
     console.log (event);
     var Content = event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML = Content;
-    speak();
+    console.log(Content);
+    if(Content=="take a picture")
+    {
+        console.log ("taking a picture ---- ")
+        speak();
+    }
 }
 
 function speak()
 {
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking a picture in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        img_id = "selfie1";
+        take_snapshot();
+        speak_data = "Taking your Selfie in 5 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 5000);
+
+    setTimeout(function()
+    {
+        img_id = "selfie2";
+        take_snapshot();
+        speak_data = "Taking your next Selfie in 5 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 5000);
+
+    setTimeout(function()
+    {
+        img_id = "selfie3";
+        take_snapshot();
+        speak_data = "Taking your next Selfie in 5 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 5000);
+
+    setTimeout(function()
+    {
+        img_id = "selfie4";
+        take_snapshot();
+        speak_data = "Taking your next Selfie in 5 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 5000);
 }
 
 Webcam.set 
@@ -36,3 +78,35 @@ Webcam.set
     png_quality:100
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) 
+    {
+        if(img_id=="selfie1")
+        {
+        document.getElementById("result1").innerHTML = '<img id="selfie1" src=" ' + data_uri + '">';
+        }
+        if(img_id=="selfie2")
+        {
+        document.getElementById("result2").innerHTML = '<img id="selfie2" src=" ' + data_uri + '">';
+        }
+        if(img_id=="selfie3")
+        {
+        document.getElementById("result3").innerHTML = '<img id="selfie3" src=" ' + data_uri + '">';
+        }
+        if(img_id=="selfie4")
+        {
+        document.getElementById("result4").innerHTML = '<img id="selfie4" src=" ' + data_uri + '">';
+        }
+
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
